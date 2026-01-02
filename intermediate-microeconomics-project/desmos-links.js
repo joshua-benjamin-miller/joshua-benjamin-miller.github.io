61c8f6b2ea6894e0229888db7bb2b40b7cc139b6
Troyever <houchengl@student.unimelb.edu.au>
2026-01-02 05:55:34 -0800

    Update desmos-links.js

--- intermediate-microeconomics-project/desmos-links.js
+++ intermediate-microeconomics-project/desmos-links.js
@@ -1,10 +1,277 @@
 const DESMOS_LINKS = {
+  // Week 1, 4 axis cost&benefit
   "4-axis-Cost-Benefit": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+  //Week1 Elasticity-Flexible-Demand
+  "Elasticity-Flexible-Demand": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+   //Week1 Elasticity-Staircase-Demand
+  "Elasticity-Staircase-Demand": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+  //Week1 Market-interventions-revised
+  "Market-interventions-revised":{
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+   //Week1 Lectorial
+  "Lectorial": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+  //Week1 Max
+  "Max": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+  //Week1 Quiz
+  "W1Quiz": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+ 
+  //Week1 Elasticity
+ "Elasticity": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+  //Week2 Consumers Problem
+  "Consumers-Problem":{
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+    //Week2 Indifference-curve
+  "Indifference-curve": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+   //Week2 Utility-Max
+  "Utility-Max":{
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+    //Week2 Discrete-Demand-Supply
+  "Discrete-Demand-Supply":{
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+ //Week3 Budget-Line-With-Complex-Pricing
+"Budget-Line-With-Complex-Pricing": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+  //Week3 Consumers-Problem-Lecture-Example
+  "Consumers-Problem-Lecture-Example": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+   //Week3 Inferior-Good
+  "Inferior-Good": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+  //Week3 Perfect-Complements
+  "Perfect-Complements":{
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+   //Week3 Quasi-Linear-Optimization
+  "Quasi-Linear-Optimization": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+  //Week3 Revealed-Preference
+  "Revealed-Preference": {
     url: "https://www.desmos.com/calculator/gaqik8ya09",
     title: "Open in Desmos"
   },
 
- "Elasticity-Flexible-Demand": {
-    url: "https://www.desmos.com/calculator/cgu6yyp5gn",
+  //Week3 Utility-Max-CES
+  "Utility-Max-CES": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
     title: "Open in Desmos"
   },
+  
+  //Week3 Sales-vs-Income-Tax
+ "Sales-vs-Income-Tax": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+   //Week4 Aggregate-Demand
+  "Aggregate-Demand": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+     //Week4 Consumers-Problem-HW
+  "Consumers-Problem-HW": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+     //Week4 Curves&Demand
+  "Curves&Demand": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+     //Week4 Curves-Expansion-Demand
+  "SCurves-Expansion-Demand":{
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+     //Week4 Ex-2023-Q3
+  "Ex-2023-Q3": {
+    url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+     //Week4 IE-SE-CobbDouglas
+  "IE-SE-CobbDouglas": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+     //Week4 IE-SE-Quasilinear
+  "IE-SE-Quasilinear": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+     //Week4 Intertemporal-consumption
+  "Intertemporal-consumption": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+     //Week4 Time-IE-SE
+  "Time-IE-SE": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+     //Week4 Consumer-Optimization
+  "Consumer-Optimization": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+   //Week5 Edgeworth-Box
+  "Edgeworth-Box": {
+    url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+  //Week5 Contract Curve
+  "Contract-Curve": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+   //Week5 Contract Curve Complements
+  "Contract-Curve-Complements": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+   //Week5 Weird-Edgeworth-Box
+  "Weird-Edgeworth-Box": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+   //Week5 Supply-Demand-From-Edgeworth
+  "Supply-Demand-From-Edgeworth": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+  //Week6 Eckel-Grossman
+  "Eckel-Grossman": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+      //Week6 Risk-Averse-Seeking-Neutral
+  "Risk-Averse-Seeking-Neutral":{
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+  //Week6 BombRisk
+  "BombRisk": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+  //Week6 CRRA-Peter-Wakker
+  "CRRA-Peter-Wakker":{
+    url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+  //Week6 St-Petersburg-Paradox
+  "St-Petersburg-Paradox": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+    //Week7 Production-returns-to-scale
+  "Production-returns-to-scale":{
+    url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+   //Week8 Costs-in-short-and-long-run
+  "Costs-in-short-and-long-run":{
+    url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+  //Week8  Costs-in-short-and-long-run-4th-panel
+  " Costs-in-short-and-long-run-4th-panel": {
+  url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+
+//Week8  Profit-maximization+supply-demand
+  " Profit-maximization+supply-demand":{
+    url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+    //Week9  Cost-Revenue-illustration
+  "Cost-Revenue-illustration": {
+   url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+  
+    //Week9  Production-Profit-CobbDouglas
+  "Production-Profit-CobbDouglas": {
+    url: "https://www.desmos.com/calculator/gaqik8ya09",
+    title: "Open in Desmos"
+  },
+};
