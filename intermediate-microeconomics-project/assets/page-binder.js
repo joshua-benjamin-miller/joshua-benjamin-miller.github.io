d9a8a0f2be09a1c008f483a7da90057b513d004c
Troyever <houchengl@student.unimelb.edu.au>
2026-09-11 03:50:35 +1000

    Update page-binder.js

--- intermediate-microeconomics-project/assets/page-binder.js
+++ intermediate-microeconomics-project/assets/page-binder.js
@@ -244,8 +244,6 @@ if (videoEl) {
   }
 }
     }
-} // closes if (videoEl)
-} // closes async function bindPage()
 
 // Run whether the script loads before or after DOMContentLoaded
 if (document.readyState === "loading") {
